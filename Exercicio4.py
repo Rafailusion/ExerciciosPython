bcfe79f2479bc6f0db0bd03b598b6ae0c9ee67f3
Rafael César Prestes Raposo <116605939+Rafailusion@users.noreply.github.com>
2022-11-19 00:52:00 -0300

    Update Exercicio4.py

--- Exercicio4.py
+++ Exercicio4.py
@@ -14,13 +14,13 @@ class Pessoa:
         if self.idade < 21:
             self.altura += 0.5
 
-Rafaella = Pessoa(nome = 'Rafaella', idade = 10, peso = 70.0, altura = 1.78)
-print(f'Nome: {Rafaella.nome}, Idade: {Rafaella.idade}, Peso: {Rafaella.peso}, Altura: {Rafaella.altura}')
-while Rafaella.idade < 80:
-    Rafaella.envelhecer()
-    Rafaella.engordar()
-    Rafaella.emagrecer()
-    Rafaella.crescer()
-    print(f'Nome: {Rafaella.nome}, Idade: {Rafaella.idade}, Peso: {Rafaella.peso}, Altura: {round(Rafaella.altura,2)}')
+Rafael = Pessoa(nome = 'Rafael', idade = 10, peso = 70.0, altura = 1.78)
+print(f'Nome: {Rafael.nome}, Idade: {Rafael.idade}, Peso: {Rafael.peso}, Altura: {Rafael.altura}')
+while Rafael.idade < 80:
+    Rafael.envelhecer()
+    Rafael.engordar()
+    Rafael.emagrecer()
+    Rafael.crescer()
+    print(f'Nome: {Rafael.nome}, Idade: {Rafael.idade}, Peso: {Rafael.peso}, Altura: {round(Rafael.altura,2)}')
 
 
